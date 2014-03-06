3e11279b9ea45d55630a52033def061d03b50726
alevicki <alevicki@gmail.com>
2014-03-06 14:20:17 -0600

    remove unnecessary services

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('ionicParseApp',
-        [ 'ionic', 'ionicParseApp.services', 'ionicParseApp.controllers' ]
+        [ 'ionic', 'ionicParseApp.controllers' ]
     )
     .config(function ($stateProvider, $urlRouterProvider) {
 
