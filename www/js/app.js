241aad0ef6f0b2b5cfc025ded8ac1863f151cef4
William Youmans <will@williamyoumans.com>
2015-02-13 23:17:28 -0500

    corrected comments

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,9 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'ionicParseApp' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
+// 'ionicParseApp.controllers' is found in controllers.js
 angular.module('ionicParseApp',
         [ 'ionic', 'ionicParseApp.controllers' ]
     )
